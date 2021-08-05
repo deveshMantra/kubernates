445f398981ef9b7fcb7ac4f93955b250b6fa0cc4
Devesh <mailme.devmehta@gmail.com>
2021-08-05 08:15:19 +0530

    updated deploy mongodb file

--- sampleYaml/mongodb/deploymongodb.sh
+++ sampleYaml/mongodb/deploymongodb.sh
@@ -1,3 +1,5 @@
+kubectl apply -f ../secret/mongodb_secrets.yaml
 kubectl apply -f mongodb_deployment.yaml
 kubectl apply -f mongodb_service.yaml
-kubectl apply -f mongoDbServiceNodePort.yaml
+# commenting as now we dont need node port for it
+# kubectl apply -f mongo_db_service_nodeport.yaml
